10a4dc11ad80059ec15aed1422d29249f68d085a
System GP <sistemas.gp1@outlook.com>
2022-11-23 16:59:29 -0500

    Modulo clientes finalizado y nuevo modulo documentos

---
+++ PROVTGVT002.php
@@ -0,0 +1,20 @@
+<?php
+
+	session_start();
+
+	if(isset($_SESSION["idusuario"])){
+
+		if ($_SESSION["superadmin"] != "S") {
+			include "view/header.html";
+			include "view/PROVTGVT002.html";
+		} else {
+			include "view/headeradmin.html";
+			include "view/PROVTGVT002.html";
+		}
+
+		include "view/footer.html";
+	} else {
+		header("Location:index.html");
+	}
+		
+
