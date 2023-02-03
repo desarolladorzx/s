ecc0248774a61ff56824e318af5605ab62c2202a
System GP <sistemas.gp1@outlook.com>
2023-02-03 17:42:46 -0500

    Modulos documentacion

---
+++ FOOFZGFZ004.php
@@ -0,0 +1,18 @@
+<?php
+
+	session_start();
+
+	if(isset($_SESSION["idusuario"])){
+
+		if ($_SESSION["superadmin"] != "S") {
+			include "view/header.html";
+			include "view/FOOFZGFZ004.html";
+		} else {
+			include "view/headeradmin.html";
+			include "view/FOOFZGFZ004.html";
+		}
+
+		include "view/footer.html";
+	} else {
+		header("Location:index.html");
+	}
---
+++ FOORHGRH002.php
@@ -0,0 +1,18 @@
+<?php
+
+	session_start();
+
+	if(isset($_SESSION["idusuario"])){
+
+		if ($_SESSION["superadmin"] != "S") {
+			include "view/header.html";
+			include "view/FOORHGRH002.html";
+		} else {
+			include "view/headeradmin.html";
+			include "view/FOORHGRH002.html";
+		}
+
+		include "view/footer.html";
+	} else {
+		header("Location:index.html");
+	}
