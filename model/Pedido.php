e1aae257e7be2bbb80661ff43e5a018e716a856a
PC-DESARROLLADOR WEB <desarrollador@grupopuma.pe>
2023-03-08 13:39:19 -0500

    se agrego sucursal a la tabla de articulos en el modulo pedido

--- model/Pedido.php
+++ model/Pedido.php
@@ -381,12 +381,16 @@ class Pedido
 	public function ListarDetalleIngresos($idsucursal)
 	{
 		global $conexion;
-		$sql = "SELECT distinct di.iddetalle_ingreso, di.stock_actual, a.nombre as Articulo, di.codigo, di.serie, di.precio_ventapublico, a.imagen, i.fecha,c.nombre as marca, um.nombre as presentacion,di.idarticulo AS idarticulo 
-			from ingreso i inner join detalle_ingreso di on di.idingreso = i.idingreso
-			inner join articulo a on di.idarticulo = a.idarticulo
-			inner join categoria c on a.idcategoria = c.idcategoria
-            inner join unidad_medida um on a.idunidad_medida = um.idunidad_medida
-			where i.estado = 'A' and i.idsucursal =$idsucursal and di.stock_actual > 0 order by fecha asc";
+		$sql = "SELECT distinct di.iddetalle_ingreso, di.stock_actual, a.nombre as Articulo, di.codigo, di.serie, di.precio_ventapublico, a.imagen, i.fecha,c.nombre as marca, um.nombre as presentacion,di.idarticulo AS idarticulo,
+		i.idsucursal,razon_social  
+					from ingreso i inner join detalle_ingreso di on di.idingreso = i.idingreso
+					inner join articulo a on di.idarticulo = a.idarticulo
+					inner join categoria c on a.idcategoria = c.idcategoria
+					inner JOIN sucursal ON sucursal.idsucursal=i.idsucursal
+					inner join unidad_medida um on a.idunidad_medida = um.idunidad_medida
+					where i.estado = 'A' 
+					-- and i.idsucursal =$idsucursal 
+					and di.stock_actual > 0 order by fecha asc";
 		$query = $conexion->query($sql);
 		return $query;
 	}
