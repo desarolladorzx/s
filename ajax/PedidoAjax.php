6d8745a6e47771523e9345224354243ec4fe6cd8
System GP <sistemas.gp1@outlook.com>
2023-04-21 15:55:22 -0500

    Cambios en label

--- ajax/PedidoAjax.php
+++ ajax/PedidoAjax.php
@@ -265,11 +265,11 @@ switch ($_GET["op"]) {
 
             $fecha_cotizacion='';
             if(strlen($reg->fecha)>0){
-                $fecha_cotizacion="<p><b>f. pedido : </b> $reg->fecha</p><p>";
+                $fecha_cotizacion="<p>$reg->fecha <b>| EV</b></p><p>";
             }
             $fecha_aprobacion='';
             if(strlen($reg->fecha_apro_coti)>0){
-                $fecha_aprobacion="<p><b>f. pedido : </b> $reg->fecha_apro_coti</p><p>";
+                $fecha_aprobacion="<p>$reg->fecha_apro_coti <b>| JV</b></p><p>";
             }
 
 
