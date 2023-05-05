c2a0479db55ce5b5322dc09aacb7a80807cc3510
DESARROLLADOR <desarrollador@grupopuma.pe>
2023-05-05 18:41:13 -0500

    se actualizo el modulo de cuentas bancarias

--- model/CuentaBancaria.php
+++ model/CuentaBancaria.php
@@ -3,9 +3,28 @@
 	require "Conexion.php";
 
 	class CuentaBancaria{
+
+		public function traerTipoCuenta()
+		{
+			global $conexion;
+			$sql = "SELECT * from tipo_cuenta ";
+			$query = $conexion->query($sql);
+			return $query;
+		}
+
+		public function traerBanco()
+		{
+			global $conexion;
+			$sql = "SELECT * from banco where activo='1'";
+			$query = $conexion->query($sql);
+			return $query;
+		}
+
 		public function listar(){
 			global $conexion;
-			$sql = "SELECT * from cuenta_bancaria where estado='A'";
+			$sql = "SELECT * ,tipo_cuenta.descripcion tipo_cuenta,banco.descripcion banco ,banco_cuenta.estado estado,banco_cuenta.descripcion banco_cuenta ,banco_cuenta.numero ,banco_cuenta.cci,banco_cuenta.balance_inicial  FROM banco_cuenta 
+			JOIN tipo_cuenta ON tipo_cuenta.idtipo_cuenta=banco_cuenta.idtipo_cuenta
+			JOIN banco ON banco.idbanco=banco_cuenta.idbanco ";
 			$query = $conexion->query($sql);
 
 			return $query;
