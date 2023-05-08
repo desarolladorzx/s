29cfb0dfaa24da973d706003500edfbefc4e5076
DESARROLLADOR <desarrollador@grupopuma.pe>
2023-05-08 13:20:37 -0500

    se modifico el modulo cuenta bancaria

--- ajax/CuentaBancariaAjax.php
+++ ajax/CuentaBancariaAjax.php
@@ -5,6 +5,21 @@ $obj = new CuentaBancaria();
 
 switch ($_GET["op"]) {
 
+
+    case 'traerTipoDivisa':
+
+
+        $query_Tipo = $obj->traerTipoDivisa();
+
+        $nuevo = array();
+        while ($reg = $query_Tipo->fetch_object()) {
+            $nuevo[] = $reg;
+        }
+        echo  json_encode($nuevo);
+
+
+        break;
+
     case 'traerTipoCuenta':
 
 
@@ -20,7 +35,7 @@ switch ($_GET["op"]) {
         break;
 
     case 'traerBanco':
- 
+
 
         $query_Tipo = $obj->traerBanco();
 
@@ -43,15 +58,25 @@ switch ($_GET["op"]) {
             $data[] = array(
                 "0" => $i,
                 "1" => $reg->tipo_cuenta,
-                "2" => $reg->estado,
+                "2" => $reg->estado==1?'activo':'inactivo',
                 "3" => $reg->banco,
                 "4" => $reg->banco_cuenta,
                 "5" => $reg->numero,
                 "6" => $reg->cci,
                 "7" => $reg->balance_inicial,
 
-                "8" => '<button class="btn btn-warning" data-toggle="tooltip" title="Editar" onclick="cargarDataCuentaBancaria(' . $reg->idbanco_cuenta . ',\'' . $reg->banco_cuenta . '\'
-                ,\'' . $reg->numero . '\'
+                "8" => '<button class="btn btn-warning" data-toggle="tooltip" title="Editar" onclick="cargarDataCuentaBancaria(
+                    `' . $reg->idbanco_cuenta . '`,
+                    `' . $reg->tipo_moneda . '`,
+                    `' . $reg->idtipo_cuenta . '`,
+                    `' . $reg->idbanco . '`,
+                    `' . $reg->nombre_titular . '`,
+                    `' . $reg->nombre_banco_cuenta . '`,
+                    `' . $reg->numero . '`,
+                    `' . $reg->cci . '`,
+                    `' . $reg->descripcion . '`,
+                    `' . $reg->balance_inicial . '`
+              
                 )"><i class="fa fa-pencil"></i> </button>&nbsp;' .
                     '<button class="btn btn-danger" data-toggle="tooltip" title="Eliminar" onclick="eliminarCuentaBancaria(' . $reg->idbanco_cuenta . ')"><i class="fa fa-trash"></i> </button>',
             );
@@ -70,8 +95,16 @@ switch ($_GET["op"]) {
 
         if (empty($_POST["txtIdcuenta_bancaria"])) {
             if ($obj->Registrar(
+                $_POST["txtNombreCuentaBancaria"],
+                $_POST["cboModTipo_de_Cuenta"],
+                $_POST["cboModIdBanco"],
+                $_POST["txtNumero"],
+                $_POST["txtNumeroCCI"],
+                $_POST["txtSaldoInicial"],
+                $_POST["cboModTipo_Moneda"],
                 $_POST["txtDescripcion"],
-                $_POST["txtNumero"]
+                $_POST["txtNombreCuentaBancaria"],
+
             )) {
                 echo "Registrado Exitosamente";
             } else {
@@ -80,8 +113,15 @@ switch ($_GET["op"]) {
         } else {
             if ($obj->Modificar(
                 $_POST["txtIdcuenta_bancaria"],
+                $_POST["txtNombreCuentaBancaria"],
+                $_POST["cboModTipo_de_Cuenta"],
+                $_POST["cboModIdBanco"],
+                $_POST["txtNumero"],
+                $_POST["txtNumeroCCI"],
+                $_POST["txtSaldoInicial"],
+                $_POST["cboModTipo_Moneda"],
                 $_POST["txtDescripcion"],
-                $_POST["txtNumero"]
+                $_POST["txtNombreCuentaBancaria"],
             )) {
                 echo "Registrado Exitosamente";
             } else {
--- model/CuentaBancaria.php
+++ model/CuentaBancaria.php
@@ -3,7 +3,13 @@
 	require "Conexion.php";
 
 	class CuentaBancaria{
-
+		public function traerTipoDivisa()
+		{
+			global $conexion;
+			$sql = "SELECT * from tipo_divisa ";
+			$query = $conexion->query($sql);
+			return $query;
+		}
 		public function traerTipoCuenta()
 		{
 			global $conexion;
@@ -22,34 +28,95 @@
 
 		public function listar(){
 			global $conexion;
-			$sql = "SELECT * ,tipo_cuenta.descripcion tipo_cuenta,banco.descripcion banco ,banco_cuenta.estado estado,banco_cuenta.descripcion banco_cuenta ,banco_cuenta.numero ,banco_cuenta.cci,banco_cuenta.balance_inicial  FROM banco_cuenta 
-			JOIN tipo_cuenta ON tipo_cuenta.idtipo_cuenta=banco_cuenta.idtipo_cuenta
-			JOIN banco ON banco.idbanco=banco_cuenta.idbanco ";
+			$sql = "SELECT * ,tipo_cuenta.descripcion tipo_cuenta,banco.descripcion banco ,banco_cuenta.estado estado,banco_cuenta.descripcion banco_cuenta ,
+			banco_cuenta.numero ,banco_cuenta.cci,banco_cuenta.balance_inicial  ,tipo_divisa.simbolo
+			FROM banco_cuenta 
+						
+						JOIN tipo_divisa ON tipo_divisa.idtipo_divisa=banco_cuenta.tipo_moneda
+						JOIN tipo_cuenta ON tipo_cuenta.idtipo_cuenta=banco_cuenta.idtipo_cuenta
+						JOIN banco ON banco.idbanco=banco_cuenta.idbanco 
+
+						where estado='1'
+			;";
 			$query = $conexion->query($sql);
 
 			return $query;
 		}
 
-		public function Eliminar($idcuentaBancaria){
+		public function Eliminar($idbanco_cuenta){
 			global $conexion;
-			$sql = "UPDATE cuenta_bancaria set estado='C' WHERE idcuenta_bancaria = $idcuentaBancaria";
+			$sql = "UPDATE banco_cuenta set estado=0 WHERE idbanco_cuenta = $idbanco_cuenta";
 			$query = $conexion->query($sql);
-
+		
 			return $query;
 		}
-		public function Registrar($descripcion,$numero){
+		public function Registrar(
+			$nombre_banco_cuenta ,
+			$idtipo_cuenta ,
+			$idbanco ,
+			$numero ,
+			$cci ,
+			$balance_inicial ,
+			$tipo_moneda ,
+			$descripcion ,
+			$nombre_titular 
+
+			){
 			global $conexion;
-			$sql = "INSERT INTO cuenta_bancaria(descripcion,estado,numero)values('$descripcion','A','$numero') ";
+			$sql = "INSERT INTO 
+			banco_cuenta
+			(
+			nombre_banco_cuenta ,
+			idtipo_cuenta ,
+			idbanco ,
+			numero ,
+			cci ,
+			balance_inicial ,
+			tipo_moneda ,
+			descripcion ,
+			nombre_titular,
+			estado 
+			)
+			values(
+			'$nombre_banco_cuenta' ,
+			'$idtipo_cuenta' ,
+			'$idbanco' ,
+			'$numero' ,
+			'$cci' ,
+			'$balance_inicial' ,
+			'$tipo_moneda' ,
+			'$descripcion' ,
+			'$nombre_titular',
+			'1'
+			) ";
 			$query = $conexion->query($sql);
 
 			// echo $sql;
 			return $query;
 		}
-		public function Modificar($idbanco,$descripcion,$numero){
+		public function Modificar(
+			$idbanco_cuenta,
+			$nombre_banco_cuenta ,
+			$idtipo_cuenta ,
+			$idbanco ,
+			$numero ,
+			$cci ,
+			$balance_inicial ,
+			$tipo_moneda ,
+			$descripcion ,
+			$nombre_titular ){
 			global $conexion;
-			$sql = "UPDATE cuenta_bancaria set descripcion='$descripcion' ,
-            numero='$numero'
-            WHERE idcuenta_bancaria = $idbanco";
+			$sql = "UPDATE banco_cuenta set nombre_banco_cuenta='$nombre_banco_cuenta' ,
+			idtipo_cuenta='$idtipo_cuenta',
+			idbanco= '$idbanco',
+			numero='$numero',
+			cci ='$cci',
+			balance_inicial='$balance_inicial' ,
+			tipo_moneda='$tipo_moneda',
+			descripcion='$descripcion',
+			nombre_titular='$nombre_titular'
+            WHERE idbanco_cuenta = $idbanco_cuenta";
+			
 			$query = $conexion->query($sql);
 			return $query;
 		}
