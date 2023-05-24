1afe83318efbe00ea8e7a9c159adae9bade259d9
DESARROLLADOR <desarrollador@grupopuma.pe>
2023-05-24 09:05:16 -0500

    se arreglo la cantidad  de stock

--- ajax/ArticuloAjax.php
+++ ajax/ArticuloAjax.php
@@ -124,8 +124,8 @@
 					"5"=>$reg->descripcion,
 					"7"=>$reg->stock_min,
 					"10"=>$reg->totalSucursal1,
-					"8"=>$reg->totalSucursal2,
-					"9"=>$reg->sumTotal,
+					"9"=>$reg->totalSucursal2,
+					"8"=>$reg->sumTotal,
 					
 					"6"=>'<img width=100px height=100px src="./'.$reg->imagen.'" />',
 					"11"=>'<button class="btn btn-warning" data-toggle="tooltip" title="Editar" onclick="cargarDataArticulo('.$reg->idarticulo.',\''.$reg->idcategoria.'\',\''.$reg->idmarca.'\',\''.$reg->idunidad_medida.'\',\''.$reg->nombre.'\',\''.$reg->descripcion.'\',\''.$reg->imagen.'\'
